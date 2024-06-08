2899bd10372dee963a519302ca035f73fdf87a67
Jon Billingsley <jonbillingsley@gmail.com>
2024-06-08 08:46:07 -0700

    reorganize

--- stow-files/emacs/dot-config/emacs/init.el
+++ stow-files/emacs/dot-config/emacs/init.el
@@ -198,7 +198,7 @@
 
 (defun jon/org-babel-tangle-config ()
   (when (equal (file-name-directory (directory-file-name buffer-file-name))
-               (concat (getenv "HOME") "/dotfiles/"))
+               (concat (getenv "HOME") "/config/"))
   (let ((org-confirm-babel-evaluate nil))
     (org-babel-tangle))))
 
@@ -227,16 +227,3 @@
 
 (use-package doom-modeline
   :init (doom-modeline-mode 1))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(doom-modeline doom-themes vundo magit visual-fill-column which-key general evil-collection evil ivy-rich counsel swiper simpleclip helpful)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
