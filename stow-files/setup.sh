4e2109666233e4748b002bc84e3cdc5ccf8b4b18
Jon Billingsley <jonbillingsley@gmail.com>
2024-06-08 21:16:43 -0700

    bump

--- stow-files/setup.sh
+++ stow-files/setup.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# $home
-echo "stowing into \$home"
 stow -v -R --dotfiles --no-folding -t ~ emacs
 stow -v -R --dotfiles --no-folding -t ~ sway
 stow -v -R --dotfiles --no-folding -t ~ waybar
 stow -v -R --dotfiles --no-folding -t ~ mako
 stow -v -R --dotfiles --no-folding -t ~ foot
-
-# /etc
-echo "stowing into /etc"
-sudo stow -v -R --no-folding -t /etc nm-no-rand-mac-addr
