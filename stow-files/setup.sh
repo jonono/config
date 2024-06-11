508717b178e67c1d02d844a3b44da4145abc0697
Jon Billingsley <jonbillingsley@gmail.com>
2024-06-10 20:53:02 -0700

    swap out mako for dunst

--- stow-files/setup.sh
+++ stow-files/setup.sh
@@ -3,5 +3,5 @@
 stow -v -R --dotfiles --no-folding -t ~ emacs
 stow -v -R --dotfiles --no-folding -t ~ sway
 stow -v -R --dotfiles --no-folding -t ~ waybar
-stow -v -R --dotfiles --no-folding -t ~ mako
 stow -v -R --dotfiles --no-folding -t ~ wezterm
+stow -v -R --dotfiles --no-folding -t ~ dunst
