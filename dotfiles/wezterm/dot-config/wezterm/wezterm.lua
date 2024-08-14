03e77bffcb662087751b242a9941b6f542179469
Jon Billingsley <git@jonono.ca>
2024-08-13 20:04:56 -0700

    lil bump

--- dotfiles/wezterm/dot-config/wezterm/wezterm.lua
+++ dotfiles/wezterm/dot-config/wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local config = {}
 
 config.font = wezterm.font 'FiraCode'
 config.font_size = 10.0
-config.color_scheme = 'Catppuccin Mocha'
+config.color_scheme = 'Ayu Dark (Gogh)'
 --config.enable_tab_bar = false
 
 return config
