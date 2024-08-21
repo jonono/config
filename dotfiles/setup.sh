63fbbf83c6ece617ec39e8b09128ff4ad31db6e9
Jon Billingsley <git@jonono.ca>
2024-08-20 22:13:13 -0700

    update config

--- dotfiles/setup.sh
+++ dotfiles/setup.sh
@@ -4,4 +4,4 @@ stow -v -R --dotfiles --no-folding -t ~ vim
 stow -v -R --dotfiles --no-folding -t ~ doomemacs
 stow -v -R --dotfiles --no-folding -t ~ git
 stow -v -R --dotfiles --no-folding -t ~ bash
-stow -v -R --dotfiles --no-folding -t ~ kitty
+stow -v -R --dotfiles --no-folding -t ~ alacritty
